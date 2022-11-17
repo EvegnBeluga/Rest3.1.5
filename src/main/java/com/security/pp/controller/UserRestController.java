7d710328748de8753bcf526a62a6cc3587b58ef9
Evgeniy Kozevnikov <104453884+EvegnBeluga@users.noreply.github.com>
2022-11-17 18:42:30 +0500

    3.1.5 F

--- src/main/java/com/security/pp/controller/UserRestController.java
+++ src/main/java/com/security/pp/controller/UserRestController.java
@@ -1,21 +1,25 @@
 package com.security.pp.controller;
 
-import com.security.pp.model.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserRestController {
 
-    @GetMapping()
-    public ResponseEntity<User> getUser(Authentication auth) {
-        return new ResponseEntity<>((User) auth.getPrincipal(),
-                HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public Principal user(Principal user) {
+        return user;
     }
+
+//    @GetMapping()
+//    public ResponseEntity<User> getUser(Authentication auth) {
+//        return new ResponseEntity<>((User) auth.getPrincipal(),
+//                HttpStatus.OK);
+//    }
 
 }
